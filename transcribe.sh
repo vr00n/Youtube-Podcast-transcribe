850ae29ab4e06f29ae03b2659b83a960dac95a24
Varun <vr00n@users.noreply.github.com>
2017-07-27 20:25:11 -0400

    Update transcribe.sh

--- transcribe.sh
+++ transcribe.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 time=1
-youtube-dl -o 'audio.%(ext)s' --extract-audio --audio-format wav $1
+if [[ $1 == 'file' ]]
+then
+wget $2
+echo "Converting file to audio.wav"
+ffmpeg -i $2 -ac 2 -f wav audio.wav
+elif [[ $1 == 'youtube' ]]
+then
+youtube-dl -o 'audio.%(ext)s' --extract-audio --audio-format wav $2
+else
+echo "use either file or youtube as arguments"
+fi
 ffmpeg -i audio.wav -f segment -segment_time 30 -c copy audio-%03d.wav
-rm audio.wav
-for i in `ls -1 audio*.wav`
+# if you want to remove the converted wav file
+#rm audio.wav
+for i in `ls -1 audio-*.wav`
 do
 num=`echo $i | awk -F. '{print $1}'| tail -c 4`
 num="$((10#$num * 1))"
@@ -12,3 +23,5 @@ time2=$((30*($num + 1)))
 echo `gdate -d "1970-01-01 00:00:00 UTC $time1 seconds" +"%M:%S"`-`gdate -d "1970-01-01 00:00:00 UTC $time2 seconds" +"%M:%S"`>>`date +%b-%d-%y`-RESULT.TXT
 python transcribe.py $i >>`date +%b-%d-%y`-RESULT.TXT
 done
+# if you want to remove all the wav chunks
+rm audio-*.wav
